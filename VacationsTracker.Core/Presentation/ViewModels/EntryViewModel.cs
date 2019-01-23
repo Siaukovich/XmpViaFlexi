aaafed74b1a284862a52822b5856425f4c852eb9
Aliaksei Safonau <Aliaksei_Safonau@epam.com>
2019-01-23 08:46:59 +0300

    Initial commit

---
+++ VacationsTracker.Core/Presentation/ViewModels/EntryViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VacationsTracker.Core.Presentation.ViewModels
+{
+    public class EntryViewModel
+    {
+    }
+}
