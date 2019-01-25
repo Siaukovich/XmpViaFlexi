92f5e5bfc9bf57546b0eba8518873f59708c8c24
Kiryl Siaukovich <Kiryl_Siaukovich@epam.com>
2019-01-25 11:39:32 +0300

    Move Login Activity to its own directory

--- VacationsTracker.Android/Views/Login/LoginActivity.cs
+++ VacationsTracker.Android/Views/Login/LoginActivity.cs
@@ -4,28 +4,29 @@ using Android.OS;
 using Android.Views;
 using Android.Widget;
 using FlexiMvvm.Views.V7;
-using VacationsTracker.Core.Presentation.ViewModels;
 using VacationsTracker.Core.Presentation.ViewModels.Login;
 
-namespace VacationsTracker.Android.Views
+namespace VacationsTracker.Android.Views.Login
 {
     [Activity(Label = "LoginActivity", Theme = "@style/AppTheme")]
     public class LoginActivity : FlxAppCompatActivity<LoginViewModel>
     {
+        private LoginActivityViewHolder ViewHolder { get; set; }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             SetContentView(Resource.Layout.activity_login);
 
-            var loginButton = FindViewById<Button>(Resource.Id.LoginButton);
-            loginButton.Click += LoginButton_Click;
+            ViewHolder = new LoginActivityViewHolder(this);
+
+            ViewHolder.LoginButton.Click += LoginButton_Click;
         }
 
         private void LoginButton_Click(object sender, EventArgs args)
         {
-            var errorLabel = FindViewById<TextView>(Resource.Id.InvalidCredentialsText);
-            errorLabel.Visibility = ViewStates.Visible;
+            ViewHolder.InvalidCredentialsText.Visibility = ViewStates.Visible;
         }
 
     }
