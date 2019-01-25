68ccd15576a8cc67f36b9b1fd27e7e5f08073cd0
Kiryl Siaukovich <Kiryl_Siaukovich@epam.com>
2019-01-25 12:41:03 +0300

    Naming refactoring

--- VacationsTracker.Android/Views/ViewHolderGenerator.cs
+++ VacationsTracker.Android/Views/ViewHolderGenerator.cs
@@ -8,14 +8,32 @@ using Android.Widget;
 
 namespace VacationsTracker.Android.Views
 {
+    public partial class HomeActivityViewHolder
+    {
+         private readonly Activity activity;
+
+         private RecyclerView recyclerView;
+
+        public HomeActivityViewHolder( Activity activity)
+        {
+            if (activity == null) throw new ArgumentNullException(nameof(activity));
+
+            this.activity = activity;
+        }
+
+        
+        public RecyclerView RecyclerView =>
+            recyclerView ?? (recyclerView = activity.FindViewById<RecyclerView>(Resource.Id.recyclerView));
+    }
+
     public partial class LoginActivityViewHolder
     {
          private readonly Activity activity;
 
-         private TextView _InvalidCredentialsText;
-         private EditText _LoginEntry;
-         private EditText _PasswordEntry;
-         private Button _LoginButton;
+         private TextView invalidCredentialsText;
+         private EditText loginEntry;
+         private EditText passwordEntry;
+         private Button loginButton;
 
         public LoginActivityViewHolder( Activity activity)
         {
@@ -26,19 +44,53 @@ namespace VacationsTracker.Android.Views
 
         
         public TextView InvalidCredentialsText =>
-            _InvalidCredentialsText ?? (_InvalidCredentialsText = activity.FindViewById<TextView>(Resource.Id.InvalidCredentialsText));
+            invalidCredentialsText ?? (invalidCredentialsText = activity.FindViewById<TextView>(Resource.Id.invalidCredentialsText));
 
         
         public EditText LoginEntry =>
-            _LoginEntry ?? (_LoginEntry = activity.FindViewById<EditText>(Resource.Id.LoginEntry));
+            loginEntry ?? (loginEntry = activity.FindViewById<EditText>(Resource.Id.loginEntry));
 
         
         public EditText PasswordEntry =>
-            _PasswordEntry ?? (_PasswordEntry = activity.FindViewById<EditText>(Resource.Id.PasswordEntry));
+            passwordEntry ?? (passwordEntry = activity.FindViewById<EditText>(Resource.Id.passwordEntry));
 
         
         public Button LoginButton =>
-            _LoginButton ?? (_LoginButton = activity.FindViewById<Button>(Resource.Id.LoginButton));
+            loginButton ?? (loginButton = activity.FindViewById<Button>(Resource.Id.loginButton));
     }
+
+    public partial class LoginRelativeActivityViewHolder
+    {
+         private readonly Activity activity;
+
+         private TextView invalidCredentialsText;
+         private EditText loginEntry;
+         private EditText passwordEntry;
+         private Button loginButton;
+
+        public LoginRelativeActivityViewHolder( Activity activity)
+        {
+            if (activity == null) throw new ArgumentNullException(nameof(activity));
+
+            this.activity = activity;
+        }
+
+        
+        public TextView InvalidCredentialsText =>
+            invalidCredentialsText ?? (invalidCredentialsText = activity.FindViewById<TextView>(Resource.Id.invalidCredentialsText));
+
+        
+        public EditText LoginEntry =>
+            loginEntry ?? (loginEntry = activity.FindViewById<EditText>(Resource.Id.loginEntry));
+
+        
+        public EditText PasswordEntry =>
+            passwordEntry ?? (passwordEntry = activity.FindViewById<EditText>(Resource.Id.passwordEntry));
+
+        
+        public Button LoginButton =>
+            loginButton ?? (loginButton = activity.FindViewById<Button>(Resource.Id.loginButton));
+    }
+
 }
 // ReSharper restore All
