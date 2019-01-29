edb42288ee11a2cf9040cc32b05c693e1efe3737
Kiryl Siaukovich <Kiryl_Siaukovich@epam.com>
2019-01-29 10:20:39 +0300

    Fix local constants naming

--- VacationsTracker.Core/Presentation/ValueConverters/DurationValueConverter.cs
+++ VacationsTracker.Core/Presentation/ValueConverters/DurationValueConverter.cs
@@ -8,12 +8,12 @@ namespace VacationsTracker.Core.Presentation.ValueConverters
     {
         protected override ConversionResult<string> Convert((DateTime, DateTime) value, Type targetType, object parameter, CultureInfo culture)
         {
-            const string DURATION_FORMAT = "{0} - {1}";
+            const string DurationFormat = "{0} - {1}";
 
             var start = value.Item1.ToString("dd MMM").ToUpper(CultureInfo.CurrentCulture);
             var end = value.Item2.ToString("dd MMM").ToUpper(CultureInfo.CurrentCulture);
 
-            var result = string.Format(DURATION_FORMAT, start, end);
+            var result = string.Format(DurationFormat, start, end);
 
             return ConversionResult<string>.SetValue(result);
         }
