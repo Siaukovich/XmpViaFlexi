474f533fcfd5113e3d3c96783b0e5795dbaf9ab5
Kiryl Siaukovich <Kiryl_Siaukovich@epam.com>
2019-02-08 10:11:44 +0300

    Add converter for refresh time

---
+++ VacationsTracker.Core/Presentation/ValueConverters/RefreshTimeValueConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using FlexiMvvm.ValueConverters;
+
+namespace VacationsTracker.Core.Presentation.ValueConverters
+{
+    public class RefreshTimeValueConverter : ValueConverter<DateTime, string>
+    {
+        protected override ConversionResult<string> Convert(DateTime value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ConversionResult<string>.SetValue($"Last updated at: {value.ToString(CultureInfo.CurrentCulture)}");
+        }
+    }
+}
