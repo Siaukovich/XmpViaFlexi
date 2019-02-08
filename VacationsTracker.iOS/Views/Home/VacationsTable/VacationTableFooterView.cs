c036df5b16c7da13d4a4dbee258d1e14d6164e5a
Kiryl Siaukovich <Kiryl_Siaukovich@epam.com>
2019-02-08 10:31:14 +0300

    Revert table footer change

--- VacationsTracker.iOS/Views/Home/VacationsTable/VacationTableFooterView.cs
+++ VacationsTracker.iOS/Views/Home/VacationsTable/VacationTableFooterView.cs
@@ -7,18 +7,34 @@ namespace VacationsTracker.iOS.Views.Home.VacationsTable
 {
     internal class VacationTableFooterView : LayoutView
     {
+        public UIView Separator { get; private set; }
+
+        protected override void SetupSubviews()
+        {
+            base.SetupSubviews();
+
+            this.BackgroundColor = AppColors.WhiteColor;
+
+            Separator = new UIView().SetSeparatorStyle();
+        }
+
         protected override void SetupLayout()
         {
             base.SetupLayout();
 
-            this.BackgroundColor = AppColors.GrayColor;
+            this.AddLayoutSubview(Separator);
         }
 
         protected override void SetupLayoutConstraints()
         {
             base.SetupLayoutConstraints();
 
-            this.AddConstraints(this.Height().EqualTo(AppDimens.DefaultSeparatorSize));
+            this.SubviewsDoNotTranslateAutoresizingMaskIntoConstraints();
+
+            this.AddConstraints(
+                Separator.AtLeftOf(this),
+                Separator.AtRightOf(this),
+                Separator.Height().EqualTo(AppDimens.DefaultSeparatorSize));
         }
     }
 }
