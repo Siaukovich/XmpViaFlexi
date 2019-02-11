970251176c9377a925d6209f25df6618b5a6c554
Kiryl Siaukovich <Kiryl_Siaukovich@epam.com>
2019-02-11 12:15:34 +0300

    Add AppStrings class

---
+++ VacationsTracker.iOS/Themes/AppStrings.cs
@@ -0,0 +1,11 @@
+﻿namespace VacationsTracker.iOS.Themes
+{
+    internal static class AppStrings
+    {
+        public static string Save => "Save";
+
+        public static string Select => "Select";
+
+        public static string Back => "Back";
+    }
+}
